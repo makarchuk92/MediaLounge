80e432ae6b199cf2455ebb321def6f26847a6ef0
Andrei <andrei.nakarchuk@gmail.com>
2024-07-15 16:52:57 +0300

    Settings for work

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,7 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Header />
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React, { FC } from 'react'
+
+const Header: FC = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  )
+}
+
+export default Header
