9828354ff485d8ba3290ad4e9a91ba45337c12b5
Andrei <andrei.nakarchuk@gmail.com>
2024-08-05 22:49:42 +0300

    Created AuthProvider

---
+++ app/proveders/auth/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { View } from 'react-native'
+import { createContext, FC, PropsWithChildren, useEffect, useState } from 'react'
+import { IContext, TypeUserState } from './auth-provider.interface'
+import * as SplashScreen from 'expo-splash-screen'
+
+export const AuthContext = createContext({} as IContext)
+
+SplashScreen.preventAutoHideAsync()
+
+const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [user, setUser] = useState<TypeUserState>(null)
+
+  useEffect(() => {
+    let mounted = true
+
+    const checkAccessToken = async () => {
+      try {
+
+      } catch {
+
+      } finally {
+        await SplashScreen.hideAsync()
+      }
+    }
+
+    checkAccessToken()
+
+    return () => {
+      mounted = false
+    }
+
+  }, [])
+
+  return <AuthContext.Provider value={{user, setUser}}>{children}</AuthContext.Provider>
+
+}
+
+export default AuthProvider
